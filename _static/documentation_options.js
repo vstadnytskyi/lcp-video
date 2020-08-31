549c9c4df1f112df4029379472fb959139b83a8a
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-08-31 20:06:14 +0000

    Update docs after building Travis build 18 of vstadnytskyi/lcp-video
    The docs were built from the branch 'master' against the commit
    95989d8e691c9d6da13e2d7801ef579ef8ec11f8.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/vstadnytskyi/lcp-video/jobs/722856376.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.7.1/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.0.post23+g10989c5',
+    VERSION: '0.0.0.post24+g95989d8',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
